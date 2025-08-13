31e109812e0cb8dd3855a0c061cbba02a0a9fe66
Alessandro Cosentino <cosenal@gmail.com>
2025-08-13 19:07:06 +0200

    fix threshold

--- metriq_gym/benchmarks/mirror_circuits.py
+++ metriq_gym/benchmarks/mirror_circuits.py
@@ -46,7 +46,7 @@ class MirrorCircuitsData(BenchmarkData):
     expected_bitstrings: list[str]
 
 
-SUCCESS_PROBABILITY_THRESHOLD = 2.0 / 3.0
+SUCCESS_PROBABILITY_THRESHOLD = 1 / np.e
 
 
 def select_optimal_qubit_subset(topology_graph: rx.PyGraph, target_width: int) -> list[int]:
