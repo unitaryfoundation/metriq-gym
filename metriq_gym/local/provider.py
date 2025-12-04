3f13309a2edbb9d4536df5c6d30b52f5ece834ea
vprusso <vincentrusso1@gmail.com>
2025-12-04 15:17:21 -0500

    fix: show all fake device options

--- metriq_gym/local/provider.py
+++ metriq_gym/local/provider.py
@@ -12,7 +12,29 @@ class LocalProvider(QuantumProvider):
         self._devices: dict[str, LocalAerDevice] = {}
 
     def get_devices(self, **_):
-        return [self.device, *self._devices.values()]
+        devices = [self.device]
+
+        # Try to get IBM backends if QiskitRuntimeService is configured
+        try:
+            service = QiskitRuntimeService()
+            backends = service.backends()
+            # Add available backends to the list
+            for backend in backends:
+                device_id = backend.name
+                if device_id != "aer_simulator":
+                    try:
+                        # Get or create the device (this will cache it in self._devices)
+                        device = self.get_device(device_id)
+                        if device not in devices:
+                            devices.append(device)
+                    except Exception:
+                        # Skip backends that can't be loaded
+                        pass
+        except Exception:
+            # QiskitRuntimeService not configured or other error - just continue
+            pass
+
+        return devices
 
     def get_device(self, device_id):
         if device_id in self._devices:
