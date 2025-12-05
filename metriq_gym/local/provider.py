af473236a7d348f94c80df9446642bd8f7fa5b93
vprusso <vincentrusso1@gmail.com>
2025-12-05 08:40:11 -0500

    chore: responding to diff comments.

--- metriq_gym/local/provider.py
+++ metriq_gym/local/provider.py
@@ -1,4 +1,4 @@
-from qbraid.runtime import QuantumProvider
+from qbraid.runtime import QuantumDevice, QuantumProvider
 from qiskit_aer import AerSimulator
 from qiskit_ibm_runtime import QiskitRuntimeService
 
@@ -6,7 +6,7 @@ from metriq_gym.local.device import LocalAerDevice
 
 
 class LocalProvider(QuantumProvider):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.device = LocalAerDevice(provider=self)
         self._devices: dict[str, LocalAerDevice] = {}
@@ -14,29 +14,28 @@ class LocalProvider(QuantumProvider):
     def get_devices(self, **_) -> list[QuantumDevice]:
         devices = [self.device]
 
-        # Try to get IBM backends if QiskitRuntimeService is configured
+        # Try to get all available IBM fake backends.
         try:
             service = QiskitRuntimeService()
             backends = service.backends()
             # Add available backends to the list
             for backend in backends:
                 device_id = backend.name
-                if device_id != "aer_simulator":
-                    try:
-                        # Get or create the device (this will cache it in self._devices)
-                        device = self.get_device(device_id)
-                        if device not in devices:
-                            devices.append(device)
-                    except Exception:
-                        # Skip backends that can't be loaded
-                        pass
+                try:
+                    # Get or create the device (this will cache it in self._devices)
+                    device = self.get_device(device_id)
+                    if device not in devices:
+                        devices.append(device)
+                except Exception:
+                    # Skip backends that can't be loaded
+                    pass
         except Exception:
             # QiskitRuntimeService not configured or other error - just continue
             pass
 
         return devices
 
-    def get_device(self, device_id) -> QuantumDevice:
+    def get_device(self, device_id: str) -> QuantumDevice:
         if device_id in self._devices:
             return self._devices[device_id]
 
@@ -44,7 +43,7 @@ class LocalProvider(QuantumProvider):
             return self.device
         try:
             aer_backend = AerSimulator.from_backend(QiskitRuntimeService().backend(device_id))
-        except Exception as exc:  # pragma: no cover - network exceptions
+        except Exception as exc:
             raise ValueError("Unknown device identifier") from exc
 
         device = LocalAerDevice(provider=self, device_id=device_id, backend=aer_backend)
